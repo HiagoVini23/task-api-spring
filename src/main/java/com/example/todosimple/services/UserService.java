57d361c5736a303ce52fc27c4014e8f2460c6a06
HiagoVini23 <hiagovini23@gmail.com>
2024-08-02 18:13:56 -0300

    feat: create task service

--- src/main/java/com/example/todosimple/services/UserService.java
+++ src/main/java/com/example/todosimple/services/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.todosimple.models.User;
-import com.example.todosimple.repositories.TaskRepository;
 import com.example.todosimple.repositories.UserRepository;
 
 @Service
@@ -17,8 +16,6 @@ public class UserService {
     
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private TaskRepository taskRepository;
 
     public User findById(Long id){
         Optional<User> user = this.userRepository.findById(id);
@@ -32,7 +29,6 @@ public class UserService {
     public User create(User obj){
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     };
 
